7ce701482a4fd4120c27d2c3c76a63026e9679e6
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-21 19:17:01 +0800

    Functions: class and methods (operations)

--- functions_cal.py
+++ functions_cal.py
@@ -1,1 +1,17 @@
+import operator
+
+# Create class
+class Calculator:
+    # Method add/plus
+    def add(self, num1, num2):
+        return operator.add(num1, num2)
+    # Method substract/minus
+    def subtract(self, num1, num2):
+        return operator.sub(num1, num2)
+    # Method multiplication/times
+    def multiply(self, num1, num2):
+        return operator.mul(num1, num2)
+    # Method division/divide
+    def divide(self, num1, num2):
+        return operator.truediv(num1, num2)
 
