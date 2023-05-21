a205750278dd5032aa8f19affa7095341d38aa9b
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-05-21 19:54:27 +0800

    pseudocodes for user interface

---
+++ user_interface.py
@@ -0,0 +1,11 @@
+# Import the Calculator class from functions_cal.py file
+
+# Import pyfiglet for ASCII art text
+
+# ANSI color codes for colored text
+
+# Create a UserInterface class
+
+# Create an instance of the UserInterface class
+
+# Run the calculator using the user interface
