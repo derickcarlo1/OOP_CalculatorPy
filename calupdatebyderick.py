29290e707d03a1a0a30ba7aac5bb81c76cc11dd6
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-06-03 18:18:00 +0800

    create new calass and override multiplication method

---
+++ calupdatebyderick.py
@@ -0,0 +1,8 @@
+# Import the Calculator class from functions_cal.py file
+from functions_cal import Calculator
+
+# Create a new class called ScientificCalculator
+class updatedcalbyderick(Calculator):
+    # Override the multiply method from the Calculator class
+    def multiply(self, num1, num2):
+        return num1 ** num2  # Calculate exponentiation instead of multiplication
