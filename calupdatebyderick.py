970e9a3b818985fe2599f303138e90904fe65df5
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-06-03 18:37:41 +0800

    just correcting pseudo

--- calupdatebyderick.py
+++ calupdatebyderick.py
@@ -1,7 +1,7 @@
 # Import the Calculator class from functions_cal.py file
 from functions_cal import Calculator
 
-# Create a new class called ScientificCalculator
+# Create a new class called updatedcalbyderick (overriding)
 class updatedcalbyderick(Calculator):
     # Override the multiply method from the Calculator class
     def multiply(self, num1, num2):
